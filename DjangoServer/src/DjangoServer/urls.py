474c17618688257ac4e30d8630b01ee0b011b218
Michael Clapp <clapp@alacran.divms.uiowa.edu>
2014-11-11 14:28:11 -0600

    Committing updated main page, about to change the bullying resources page. Wanted to make sure I had a backup of it

--- DjangoServer/src/DjangoServer/urls.py
+++ DjangoServer/src/DjangoServer/urls.py
@@ -10,5 +10,5 @@ urlpatterns = patterns('',
     url(r'^(?i)DataCollection/', include('DataCollection.urls',namespace="DataCollection")),
     url(r'^(?i)admin/', include(admin.site.urls)),
     url(r'^(?i)survey/',include('survey.urls',namespace="survey")),
-    url(r'^(?i)cyber-bullying/$', include('DataCollection.urls',namespace="DataCollection")),
+    url(r'^(?i)cyber-bullying/', include('DataCollection.urls',namespace="cyber-bullying")),
 )
